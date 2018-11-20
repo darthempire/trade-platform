d2a2b9f7bd9e91047f3c2f735246ca9911e1844e
Vasya-Desktop <vasyamakarchuk@outlook.com>
2018-11-21 02:01:00 +0300

    refactoring

--- index.js
+++ index.js
@@ -9,16 +9,9 @@ const bodyParser = require('body-parser');
 const jwt = require('_helpers/jwt');
 const errorHandler = require('_helpers/error-handler');
 
-
-// var authentification = require("./middlewares").authentification;
-// var permit = require("./middlewares").permit;
-
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-
-// app.use(authentification);
 
 // use JWT auth to secure the api
 app.use(jwt());
@@ -29,19 +22,6 @@ app.use('/users', require('./core/users/users.controller'));
 // global error handler
 app.use(errorHandler);
 
-
-
-
-
-// // viewing account "GET" available to account owner and account member
-// api.post("/account", permit("owner"), (req, res) => {
-//     //   res.json({ currentUser: req.user })
-//     res.write("you posted:\n");
-//     res.end(JSON.stringify(req.body, null, 2));
-// });
-
-// // mount api router
-// app.use("/api", api);
 
 // start server
 const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
