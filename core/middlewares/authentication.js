c4eead9efde08bb8f467257e16a17f5df34967af
Vasya-Desktop <vasyamakarchuk@outlook.com>
2018-11-21 00:13:45 +0300

    auth

--- core/middlewares/authentication.js
+++ core/middlewares/authentication.js
@@ -1,14 +1,16 @@
 // middleware for authentication
 module.exports = function authorize(req, res, next) {
     const apiToken = req.headers['x-api-token'];
+
     
+    console.log(1234);
+
     // get the user
     req.user = {
         name: 'Vasya',
         role: 'owner'
     };
-       
-    console.log(req.user);
+
     // always continue to next middleware
     next();
   }
--- index.js
+++ index.js
@@ -3,20 +3,20 @@ var authentification = require("./core/middlewares").authentification;
 var permit = require("./core/middlewares").permit;
 
 var app = express(),
-    api = express.Router();
+  api = express.Router();
 
-// mount api router
-app.use("/api", api);
-
+// // mount api router
+// app.use("/api", api);
 
 app.use(authentification);
 
+// viewing account "GET" available to account owner and account member
+api.get("/account", permit("employee"), (req, res) =>
+  res.json({ currentUser: req.user })
+);
 
-app.get("/", function(req, res) {
-  res.send("Hello World!");
-});
-
-api.get("/account", permit('owner'),  (req, res) => req.json({currentUser: req.user}));
+// mount api router
+app.use("/api", api);
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Example app listening on port 3000!");
